f5f73dfba4c564c53fcfb00772c78f5a676f7145
thejosephkalu <kalu@tlu.ee>
2024-05-04 12:47:03 +0300

    Class work

--- Lab7/app1/src/App.js
+++ Lab7/app1/src/App.js
@@ -1,33 +1,45 @@
-import {useState, useEffect} from 'react'; 
-import './App.css'; 
-import APIKey from './config'; 
-import Movie from './components/Movie'; 
- 
-const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key='; 
- 
-function App() { 
-  const [movieResults, setMovieResults] = useState([]); 
-  //const testArray = ['age',2,true,"aa"]; 
- 
-  useEffect( ()=> { 
-      fetch(APIURL + APIKey) 
-      .then( (resp) => resp.json() ) 
-      .then( (data) => {
-      console.log(data);
-      setMovieResults(data.results);
-    }
-      ) 
-  } 
-  ,[]) 
- 
-  return ( 
- 
-    <> 
-    {movieResults.map( (element,index) => (<Movie key={index}/>) 
-     
-    )} 
-    </> 
-  ); 
-} 
- 
+import { useState, useEffect } from "react";
+import "./App.css";
+import APIKey from "./config";
+import Movie from "./components/Movie";
+
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
+const SearchURL = "https://api.themoviedb.org/3/search/movie?api_key=";
+
+function App() {
+  const [movieResults, setMovieResults] = useState([]);
+  //const testArray = ['age',2,true,"aa"];
+
+  useEffect(() => {
+    fetch(APIURL + APIKey)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setMovieResults(data.results);
+      });
+  }, []);
+
+  return (
+    <>
+      <header>
+        <form>
+          <input
+            className="search"
+            type="search"
+            placeholder="search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          ></input>
+        </form>
+      </header>
+      <div className="movie-container">
+        {movieResults.map((element, index) => (
+          <Movie key={element.id} {...element} />
+        ))}
+      </div>
+    </>
+  );
+}
+
 export default App;
--- Lab7/app1/src/components/Movie.js
+++ Lab7/app1/src/components/Movie.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./Movie.css"
+const ImageURL = 'https://image.tmdb.org/t/p/w500/';
 
-function Movie(){
-    return(
-<div>This is Movie </div>
-
-    );
+function Movie({poster_path,title,overview, vote_average}) 
+{
+  return (
+    <div> className="movie">
+      <img src{ImageURL + poster_path} alt={title}></img>
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+      <div className="movie-overview">
+        <h2>overview</h2>
+        <p></p>
+    </div>
+  );
 }
 
 export default Movie;
