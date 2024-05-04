c4bae1133f0bc2687b48adf1f830091b68e7078f
thejosephkalu <kalu@tlu.ee>
2024-05-04 13:38:41 +0300

    Class work

--- Lab7/app1/src/App.js
+++ Lab7/app1/src/App.js
@@ -1,45 +1,64 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import APIKey from "./config";
-import Movie from "./components/Movie";
+import {useState, useEffect} from 'react'; 
+import './App.css'; 
+import APIKey from './config'; 
+import Movie from './components/Movie'; 
+ 
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key='; 
+const SearchURL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
-const APIURL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
-const SearchURL = "https://api.themoviedb.org/3/search/movie?api_key=";
 
-function App() {
-  const [movieResults, setMovieResults] = useState([]);
-  //const testArray = ['age',2,true,"aa"];
+ 
+function App() { 
+  const [movieResults, setMovieResults] = useState([]); 
+  const [searchTerm,setSearchTerm]=useState('')
+  //const testArray = ['age',2,true,"aa"]; 
+ 
+  useEffect( ()=> { 
+      fetch(APIURL + APIKey) 
+      .then( (resp) => resp.json() ) 
+      .then( (data) => {
+      console.log(data.results);
+      setMovieResults(data.results);
+    }
+      ) 
+  } 
+  ,[]) 
 
-  useEffect(() => {
-    fetch(APIURL + APIKey)
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        setMovieResults(data.results);
-      });
-  }, []);
-
-  return (
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
+    if(searchTerm){
+      fetch(SearchURL + APIKey + "&query=" + searchTerm) 
+      .then( (resp) => resp.json() ) 
+      .then( (data) => {
+      console.log(data.results);
+      setMovieResults(data.results);
+    }
+      )
+    }
+  }
+ 
+  const handleOnChange=(e) =>{
+    console.log(e.target.value)
+    setSearchTerm(e.target.value)
+  }
+  return ( 
+ 
     <>
-      <header>
-        <form>
-          <input
-            className="search"
-            type="search"
-            placeholder="search..."
-            value={searchTerm}
-            onChange={handleOnChange}
-          ></input>
-        </form>
-      </header>
-      <div className="movie-container">
-        {movieResults.map((element, index) => (
-          <Movie key={element.id} {...element} />
-        ))}
-      </div>
-    </>
-  );
-}
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input className='search' type='search' placeholder='Search...' value={searchTerm} onChange={handleOnChange}> 
 
+        </input>
+      </form>
+
+    </header>
+    <div className='movie-container'> 
+    {movieResults.map( (element) => (<Movie key={element.id} {...element}/>) 
+     
+    )} 
+    </div> 
+    </>
+  ); 
+} 
+ 
 export default App;
--- Lab7/app1/src/components/Movie.js
+++ Lab7/app1/src/components/Movie.js
@@ -1,21 +1,25 @@
 import React from "react";
-import "./Movie.css"
+import "./Movie.css";
 const ImageURL = 'https://image.tmdb.org/t/p/w500/';
 
-function Movie({poster_path,title,overview, vote_average}) 
+function Movie({poster_path,title,overview,vote_average})
 {
-  return (
-    <div> className="movie">
-      <img src{ImageURL + poster_path} alt={title}></img>
-      <div className="movie-info">
+    return(
+<div className="movie">
+    <img src={ImageURL+poster_path}alt={title}></img>
+    <div className="movie-info">
         <h3>{title}</h3>
         <span>{vote_average}</span>
-      </div>
-      <div className="movie-overview">
-        <h2>overview</h2>
-        <p></p>
     </div>
-  );
+    <div className="movie-overview">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+    </div>
+</div>
+
+    );
 }
+
+
 
 export default Movie;
